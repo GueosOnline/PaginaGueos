3ee2711f47ef4325b1da253dac2e3c3fce1bef38
Online <online@gueos.com.co>
2024-12-23 15:47:37 -0500

    Primer commit después de reiniciar el repositorio

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer class="footer text-lg-start bg-primary bg-gradient mt-auto">
+    <div class="container text-md-start pt-2 pb-1">
+        <!-- Grid row -->
+        <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-12 col-lg-3 col-sm-12 mb-2">
+                <!-- Content -->
+                <p class="text-white h3">
+                    Tienda Online CDP
+                </p>
+            </div>
+            <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+    </div>
+</footer>
